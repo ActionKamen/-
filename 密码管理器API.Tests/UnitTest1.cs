1559aa1e000e022e7b47899d27cccf8e91dfb9c9
林伟东 <532882148@qq.com>
2015-05-26 10:28:19 +0800

    初始化

---
+++ 密码管理器API.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace 密码管理器API.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ 密码管理器API/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="密码管理器API.WebApiApplication" Language="C#" %>
